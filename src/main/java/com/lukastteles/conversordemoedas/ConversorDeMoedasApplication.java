f4d5b5ec9d059bef924ce3984907c8d731d507a4
Lukas Teles <11294474+lukastteles@users.noreply.github.com>
2021-02-03 18:37:06 -0300

    Configuração inicial do projeto
    Arquivos iniciais para o projeto utilizando String Boot

---
+++ src/main/java/com/lukastteles/conversordemoedas/ConversorDeMoedasApplication.java
@@ -0,0 +1,13 @@
+package com.lukastteles.conversordemoedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDeMoedasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversorDeMoedasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukastteles/conversordemoedas/ConversorDeMoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukastteles.conversordemoedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorDeMoedasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
